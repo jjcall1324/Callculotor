0d65a4131935c42eac955641d7fb885e81bd97bf
Janae Calloway <janaecalloway06@gmail.com>
2023-06-28 13:07:54 -0500

    a working a calculator with viewbinding

--- app/src/main/java/com/example/callculotor/MainActivity.kt
+++ app/src/main/java/com/example/callculotor/MainActivity.kt
@@ -2,50 +2,102 @@ package com.example.callculotor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.util.Log
+import android.widget.TextView
 import com.example.callculotor.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
-    private var canAddOperation = false
-    private var canAddDecimal = true
     private lateinit var binding: ActivityMainBinding
+    private lateinit var workingsTV: TextView
+    private lateinit var resultsTV: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
-    fun allClearAction(binding: ActivityMainBinding){
-       binding.text = ""
+        workingsTV = binding.workingsTV
+        resultsTV = binding.resultsTV
 
 
-    }
-    fun backSpaceAction(view: View){
-        val length = workingsTV.length()
-        if (length > 0)
-            workingsTV.text = workingsTV.text.subSequence(0 ,length - 1)
-    }
-    fun numberAction(view: View){
-        if (view is Button){
-            if(view.text == "."){
-                if (canAddDecimal)
-                    workingsTV.append(view.text)
-                canAddDecimal = false
+        with(binding) {
+
+            //Numbers
+            oneTV.setOnClickListener { appendOnExpression("1", false) }
+            twoTV.setOnClickListener { appendOnExpression("2", true) }
+            threeTV.setOnClickListener { appendOnExpression("3", true) }
+            fourTV.setOnClickListener { appendOnExpression("4", true) }
+            fiveTV.setOnClickListener { appendOnExpression("5", true) }
+            sixTV.setOnClickListener { appendOnExpression("6", true) }
+            sevenTV.setOnClickListener { appendOnExpression("7", true) }
+            eightTV.setOnClickListener { appendOnExpression("8", true) }
+            nineTV.setOnClickListener { appendOnExpression("9", true) }
+            zeroTV.setOnClickListener { appendOnExpression("0", true) }
+            dotTV.setOnClickListener { appendOnExpression(".", true) }
+
+            //Operators
+            plusTV.setOnClickListener { appendOnExpression("+", false) }
+            minusTV.setOnClickListener { appendOnExpression("-", false) }
+            mulTV.setOnClickListener { appendOnExpression("*", false) }
+            divideTV.setOnClickListener { appendOnExpression("/", false) }
+
+
+            clearTV.setOnClickListener {
+                workingsTV.text = ""
+                resultsTV.text = ""
             }
-            else
-                workingsTV.append(view.text)
-            canAddOperation = true
+            backTV.setOnClickListener {
+                val string = workingsTV.text.toString()
+                if (string.isNotEmpty()) {
+                    workingsTV.text = string.substring(0, string.length - 1)
+                }
+                resultsTV.text = ""
+            }
+            equalTV.setOnClickListener {
+                try {
+                    val expression = ExpressionBuilder(workingsTV.text.toString()).build()
+                    val result = expression.evaluate()
+                    val longResult = result.toLong()
+                    if(result == longResult.toDouble())
+                        resultsTV.text = longResult.toString()
+                    else
+                        resultsTV.text = result.toString()
+
+                }catch (e:Exception){
+                    Log.d("Exception"," message : " + e.message )
+                }
+                }
+            }
+
+        }
+        private fun appendOnExpression(string: String, clearResult: Boolean){
+            if(clearResult){
+                workingsTV.text = ""
+            }
+            workingsTV.append(resultsTV.text)
+            workingsTV.append(string)
+            resultsTV.text = ""
         }
 
-    }
-    fun operationAction(view: View){
-        if (view is Button && canAddOperation) {
-            workingsTV.append(view.text)
-            canAddOperation = false
-            canAddDecimal = true
-        }
-    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
